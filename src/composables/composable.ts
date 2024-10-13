9fd98943d3f98a9b3a47ed4ec761df25a0f07f55
Alireza Ghodosi Nejad <142090759+alirezagh031@users.noreply.github.com>
2024-10-13 13:51:37 +0330

    change input in bgColorClassName and colorClassName to props

--- src/composables/composable.ts
+++ src/composables/composable.ts
@@ -1,24 +1,17 @@
-import { ref } from 'vue';
-export function roundedClassName(borderRadiusSize : string | 'none' | 'xs' | 'sm' | 'lg' | '' | 'md' | 'xl' | 'circle') {
-    const class_name = ref('roundedd-');
-    class_name.value = class_name.value + borderRadiusSize;
-    return class_name.value;
+import { ref, computed } from 'vue';
+
+export function colorClassName(props: {activeTextColor: string; onActiveTextColor: string}) {
+    const className = computed(() => {
+        return [`color-${props.activeTextColor}`, `color-${props.onActiveTextColor}`];
+    })
+    
+    return className;
 }
 
-export function sizeClassName(size : string | 'sm' | 'md' | '' | 'lg' | 'xl' | 'xs') {
-    const class_name = ref('size-');
-    class_name.value = class_name.value + size;
-    return class_name.value;
-}
-
-export function colorClassName(color : string) {
-    const class_name = ref('text-');
-    class_name.value = class_name.value + color;
-    return class_name.value;
-}
-
-export function bgColorClassName(bg_color : string) {
-    const class_name = ref('bg-');
-    class_name.value = class_name.value + bg_color;
-    return class_name.value;
+export function bgColorClassName(props: {activeColor: string; onActiveColor: string}) {
+    const className = computed(() => {
+        return [`bg-${props.activeColor}`, `bg-${props.onActiveColor}`];
+    })
+    
+    return className;
 }
