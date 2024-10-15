d051c2e6dccb29e63081f62036ecb9d0d7288225
Alireza Ghodosi Nejad <142090759+alirezagh031@users.noreply.github.com>
2024-10-15 14:14:18 +0330

    add create prop for color

--- src/composables/composable.ts
+++ src/composables/composable.ts
@@ -1,5 +1,29 @@
 import { ref, computed } from 'vue';
+export function createColorProp(defaultActiveColor: string = 'text-white', defaultOnActiveColor: string = 'text-black'){
+    return {
+        activeTextColor: {
+            type: String,
+            default: defaultActiveColor
+        },
+        onActiveTextColor: {
+            type: String,
+            default: defaultOnActiveColor
+        }
+    };
+}
 
+export function createBgColorProp(defaultBgActiveColor: string = 'bg-slate-950', defaultBgOnActiveColor: string = 'bg-slate-200'){
+    return {
+        BgActiveColor: {
+            type: String,
+            default: defaultBgActiveColor
+        },
+        BgOnActiveColor: {
+            type: String,
+            default: defaultBgOnActiveColor
+        }
+    };
+}
 export function colorClassName(props: {activeTextColor: string; onActiveTextColor: string}) {
     const className = computed(() => {
         return [`color-${props.activeTextColor}`, `color-${props.onActiveTextColor}`];
@@ -8,7 +32,7 @@ export function colorClassName(props: {activeTextColor: string; onActiveTextColo
     return className;
 }
 
-export function bgColorClassName(props: {activeColor: string; onActiveColor: string}) {
+export function bgColorClassName(props: {BgactiveColor: string; BgonActiveColor: string}) {
     const className = computed(() => {
         return [`bg-${props.activeColor}`, `bg-${props.onActiveColor}`];
     })
